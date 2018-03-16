aa10544a5b2bb9239eff6dec93251a7c9420e718
arcanosam <arcanosam@gmail.com>
2018-03-15 22:01:02 -0300

    still figuring out how to solve this assignment 03

--- a03/migrate-data.js
+++ a03/migrate-data.js
@@ -1,47 +1,49 @@
 // Assignment 03
-// Data migration tool with MongoDB
+// MongoDB Migration Node Script
 
 const fs = require('fs')
+const process = require('process')
+
 const logger = require('morgan')
 const errorhandler = require('errorhandler')
 const mongodb= require('mongodb')
+const async = require('async')
 
-let {insCustomers} = require('./cus-ins.js')
-let {updCustomers} = require('./cus-upd.js')
+let {migrationProcess} = require('./migration-process.js')
 
 const url = 'mongodb://localhost:27017/edxmsnodejs-a03'
 
+let nTasks=0
+let RECORDS=1000
+
 app.use(logger('dev'))
 
-async function migraCustomers(db) {
+let usageHelp = ()=>{
+    console.log('Assignment 03 - MongoDB Migration Node Script')
+    console.log('Usage:')
+    console.log('   node migrate-data.js [ntasks]')
+    console.log('   ntasks - number of parallel tasks used to migrate')
+    console.log('          - maximum tasks: 100')
+    console.log('          - minimum task:    1')
+}
 
-    const json_data = {
-        cust: fs.readFileSync('./m3-customer-data.json', 'utf-8'),
-        addr: fs.readFileSync('./m3-customer-data.json', 'utf-8')
-    }
+if(process.argv.length==3){
+    
+    nTasks = process.argv[2]
+    
+    if(nTasks < 1 && nTasks > 100){
 
-    try {
-        await insCustomers(
-            db, 
-            () => {
-                updCustomers(
-                    db, 
-                    () =>  {
-                        db.close()
-                    }
-                )
-            },
-            json_data
-        )
+        usageHelp()
+    }    
+}else{
 
-        console.log('Done!')
-        process.exit()
+    usageHelp()
+}
 
-    } catch(e) {
-        console.log(e);
-        process.exit();
-    }
-};
+const json_data = {
+    cust: JSON.parse(fs.readFileSync('./m3-customer-data.json', 'utf-8')),
+    addr: JSON.parse(fs.readFileSync('./m3-customer-data.json', 'utf-8'))
+}
 
 mongodb.MongoClient.connect(
     url, 
@@ -51,8 +53,17 @@ mongodb.MongoClient.connect(
             return process.exit(1)
 
         console.log('Connection is okay')
+        
+        const collection = db.collection('edxmsnodejs-a03')
 
-        migraCustomers(db)
+        async.parallel(
+            migrationProcess(nTasks,collection),
+            (error, results) => {
+                console.log(error)
+                console.log(results)
+            }
+        )
+        
     }
 )
 
